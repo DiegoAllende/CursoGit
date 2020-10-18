9f27ba46e800930585aa5f170cd581ab9b82863f
diego <diego.me11911@gmail.com>
2020-10-17 20:48:33 -0500

    pimercommit diego

---
+++ js/javascript.js
@@ -0,0 +1,15 @@
+function saludo(){
+    console.log("hola a todos");
+}
+
+function saludo2(){
+    console.log("hola a todos 2");
+}
+
+function saludo3(){
+    console.log("hola a todos 3");
+}
+
+function saludo44(){
+    console.log("hola a todos 4");
+}
